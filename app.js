13e23bdcba68fda9179a6ae98302b0bdce68fe23
Jeffrey Ercolani <Jercolani@JeffyMBP.local>
2015-04-16 11:26:49 -0700

    finish minimum requirements and adequate styling

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ var db = require('./models');
 var app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
+app.use(methodOverride("_method"));
 
 app.use(express.static(__dirname + '/public'));
 
@@ -48,13 +49,13 @@ app.use("/", function (req, res, next) {
     req.user = null;
   }
 
-  req.session.userId=1;
+req.session.userId=1;
 
   next();
 });
 
 app.get('/', function(req,res) {
-	res.render('index/index');
+	res.render('index/index', {userId: req.session.userId});
 });
 
 app.post('/', function(req,res) {
@@ -67,11 +68,11 @@ app.post('/', function(req,res) {
 });
 
 app.get('/about', function(req,res) {
-	res.render('index/about');
+	res.render('index/about', {userId: req.session.userId});
 });
 
 app.get('/contact', function(req,res) {
-	res.render('index/contact');
+	res.render('index/contact', {userId: req.session.userId});
 });
 
 app.get('/signup', function(req,res) {
@@ -83,7 +84,9 @@ app.post('/signup', function(req,res) {
 	var username = req.body.newUsername;
 	var email = req.body.newEmail;
 	var password = req.body.newPassword;
-	console.log("password is" + password);
+	var firstName = req.body.newFirstName;
+	var lastName = req.body.newLastName;
+	var age = req.body.newAge;
 	db.User.createSecure(username,email,password).then(function(user) {
 		req.login(user);
 		res.redirect('/profile');
@@ -106,18 +109,17 @@ app.post('/login', function(req,res) {
 app.get('/search', function(req,res) {
 	var name = req.query.name;
 	if (!name) {
-		res.render('breweries/search', {breweries: [], noBreweries: false})
+		res.render('breweries/search', {breweries: [], noBreweries: false, userId: req.session.userId, results: false})
 	} else {
-		var urlBdb = "http://api.brewerydb.com/v2/search?q="+name+"&type=brewery&key=024d36e33d31c96089654338402722b4";
+		var urlBdb = "http://api.brewerydb.com/v2/search?q="+name+"&type=brewery&key="+apiKey;
 		request(urlBdb, function(err, resp, body) {
 			if (!err && resp.statusCode === 200) {
 				var site = JSON.parse(body).data;
 				console.log(site);
 				if (site) {
-					res.render('breweries/search', {breweries: site, noBreweries: false});
-
+						res.render('breweries/search', {breweries: site, noBreweries: false, userId: req.session.userId, results: true});
 				} else {
-					res.render('breweries/search', {breweries: [], noBreweries: true});
+					res.render('breweries/search', {breweries: [], noBreweries: true, userId: req.session.userId, results: true});
 				}
 			}
 		});
@@ -135,7 +137,13 @@ app.post('/profile', function(req,res) {
 	res.redirect('/');
 });
 
-app.put('/profile', function(req,res) {
+app.get('/edit', function(req,res) {
+	db.User.findAll({where: {UserId: req.session.userId}}).then(function(user) {
+		res.render('edit', {firstName:user.first_name, lastName: user.last_name, age: user.age});
+	});
+});
+
+app.put('/edit', function(req,res) {
 	var firstName = req.body.firstName;
 	var lastName = req.body.lastName;
 	var city = req.body.city;
@@ -155,8 +163,15 @@ app.get('/breweries/:id', function(req,res) {
 	request(endpointDesc, function(err, resp, body) {
 		if (!err && resp.statusCode === 200) {
 			var apiDesc = JSON.parse(body).data;
-			console.log(apiDesc);
-			res.render('breweries/breweries', {brewery: apiDesc, brewId: brewId})
+			if (apiDesc.description) {
+				db.Favorite.findAll({where: {UserId: req.session.userId, breweryId: brewId}}).then(function(site) {
+					res.render('breweries/breweries', {brewery: apiDesc, brewId: brewId, noDescription: false, userId: req.session.userId});
+				});
+			} else {
+				db.Favorite.findAll({where: {UserId: req.session.userId, breweryId: brewId}}).then(function(site) {
+					res.render('breweries/breweries', {brewery: apiDesc, brewId: brewId, noDescription: true, userId: req.session.userId});
+				});
+			}
 		}
 	});
 });
@@ -164,7 +179,6 @@ app.get('/breweries/:id', function(req,res) {
 app.post('/favorites', function(req,res) {
 	var brewery = req.body.brewery;
 	db.Favorite.findAll({where: {UserId: req.session.userId, breweryId: brewery.breweryId}}).then(function(site) {
-		console.log("result is " + site)
 		if (site.length < 1) {
 			db.Favorite.create({UserId: req.session.userId, name: brewery.name, breweryId: brewery.breweryId}).then(function(beer) {
 				res.redirect('/favorites');
@@ -172,6 +186,21 @@ app.post('/favorites', function(req,res) {
 		} else {
 			res.redirect('/favorites');
 		}
+	});
+});
+
+app.delete('/favorites/:breweryId', function(req,res) {
+	req.currentUser().then(function(user){
+	var brewery = req.params.breweryId;
+	console.log(brewery);
+	db.Favorite.find({where: {breweryId: brewery}})
+	.then(function(favorite) {
+		console.log('this should be fucced', favorite);
+		// if (site.length > 0) {
+			favorite.destroy().then(function(page) {
+				res.redirect('/favorites');
+			});
+		})
 	});
 });
 
